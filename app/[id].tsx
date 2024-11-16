8846ca4f810a7b8fdaccb3c9c14cfb4342e28cf4
Moabi <ramatsuilm@outlook.com>
2024-11-16 17:22:19 +0200

    Update task

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,12 +1,27 @@
-import { useObject } from '@realm/react';
+import { useObject, useRealm } from '@realm/react';
 import { Stack, useLocalSearchParams } from 'expo-router';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import { BSON } from 'realm';
 import { Task } from '../models/Task';
+import { useState } from 'react';
 
 const TaskDetails = () => {
     const { id } = useLocalSearchParams();
     const task = useObject<Task>(Task, new BSON.ObjectID(id as string));
+
+    const [ updatedDescription, setUpdatedDescription ] = useState(task?.description);
+
+    const realm = useRealm();
+
+    const updateDescription = () => {
+      if (!task) {
+        return;
+      }
+
+      realm.write(() => {
+        task.description = updatedDescription || '';
+      });
+    }
 
     if (!task) {
       return <Text>Not Found</Text>
@@ -15,7 +30,11 @@ const TaskDetails = () => {
     return (
       <View style={{padding: 10}}>
           <Stack.Screen options={{title: 'Task Details'}}/>
-          <Text style={{color: 'white', fontSize: 20 }}>{task.description}</Text>
+          <TextInput value={updatedDescription}
+            onChangeText={setUpdatedDescription}
+            onEndEditing={() => updateDescription()}
+            style={{color: 'white', fontSize: 20 }}
+          />
       </View>
     )
 }
